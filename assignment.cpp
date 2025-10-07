6e5c64408694432204666bfa6c4e3b363ee8b974
Sakshamebdev873 <vinodarya344@gmail.com>
2025-10-07 16:55:56 +0530

    assign_1

--- assignment.cpp
+++ assignment.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
 using namespace std;
-int main (){
-cout<<"Hello";
+void binary(int arr[], int key, int low, int med, int high)
+{
+    if (low > high)
+    {
+        cout << "Key not found";
+        return;
+    }
+    if (arr[med] == key)
+    {
+        cout << "Found at index" << med << endl;
+        return;
+    }
+    else if (arr[med] > key)
+    {
+        int new_med = ((med - 1) + low) / 2;
+        binary(arr, key, low, new_med, med - 1);
+    }
+    else
+    {
+        int new_med = ((med + 1) + high) / 2;
+        binary(arr, key, med + 1, new_med, high);
+    }
+}
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5, 6, 7};
+    int key = 7;
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int low = 0;
+    int high = size - 1;
+    int med = (low + high) / 2;
+    binary(arr, key, 0, med, high);
     return 0;
 }
