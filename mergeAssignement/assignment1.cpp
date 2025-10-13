7b178f337fe3269c125fc93db9554e00d234119d
Sakshamebdev873 <vinodarya344@gmail.com>
2025-10-13 22:25:53 +0530

    mergeasiign 1

---
+++ mergeAssignement/assignment1.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+void merge( string arr[], int si, int mid, int ei)
+{
+    vector<string> temp;
+    int i = si;
+    int j = mid + 1;
+    while (i <= mid && j <= ei)
+    {
+        if (arr[i] <= arr[j])
+        {
+            temp.push_back(arr[i++]);
+        }
+        else
+        {
+            temp.push_back(arr[j++]);
+        }
+    }
+    while (i <= mid)
+    {
+        temp.push_back(arr[i++]);
+    }
+    while (j <= ei)
+    {
+        temp.push_back(arr[j++]);
+    }
+    for (int idx = si, x = 0; idx <= ei; idx++)
+    {
+        arr[idx] = temp[x++];
+    }
+}
+void mergesort(string arr[],int si,int ei){
+    if(si>=ei){
+        return;
+    }
+    int mid = (si+ei)/2;
+    mergesort(arr,si,mid);
+    mergesort(arr,mid+1,ei);
+    merge(arr,si,mid,ei);
+}
+void printArr(string arr[],int n){
+    for (int i = 0; i < n; i++)
+    {
+        cout<<arr[i]<<endl;
+    }
+    
+}
+int main()
+{
+string arr[] = { "sun", "earth", "mars", "mercury" };
+int size = sizeof(arr)/sizeof(arr[0]);
+mergesort(arr,0,size-1);
+printArr(arr,size);
+    return 0;
+}
