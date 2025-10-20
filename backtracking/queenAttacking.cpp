c8a1c73c77a89f2dd74442ff0bd417b7eaafc740
Sakshamebdev873 <vinodarya344@gmail.com>
2025-10-20 19:18:01 +0530

    queen attacking

--- backtracking/queenAttacking.cpp
+++ backtracking/queenAttacking.cpp
@@ -12,7 +12,42 @@ void printBoard(vector<vector<char>> board)
         }
         cout << endl;
     }
-    cout << "----------"<<endl;
+    cout << "----------" << endl;
+}
+bool isSafe(vector<vector<char>> board, int row, int col)
+{
+    int n = board.size();
+    // horizontal
+    for (int j = 0; j < n; j++)
+    {
+        if (board[row][j] == 'Q')
+        {
+            return false;
+        }
+    }
+    // vertical
+    for (int i = 0; i < n; i++)
+    {
+        if (board[i][col] == 'Q')
+        {
+            return false;
+        }
+    }
+    // diagonal left
+    for (int i = row, j = col; i >= 0 && j >= 0; i--, j--)
+    {
+        if (board[i][j] == 'Q')
+        {
+            return false;
+        }
+    }
+    // diagonal right
+    for(int i=row,j=col;i>=0 && j<n;i--,j++){
+        if(board[i][j]=='Q'){
+            return false;
+        }
+    }
+    return true;
 }
 void nQueens(vector<vector<char>> board, int row)
 {
@@ -24,22 +59,28 @@ void nQueens(vector<vector<char>> board, int row)
     }
     for (int j = 0; j < n; j++)
     {
-        board[row][j] = 'Q';
+       if (isSafe(board,row,j))
+       {
+     board[row][j] = 'Q';
         nQueens(board, row + 1);
-        board[row][j] = '.';
+        board[row][j] = '.';  
+    }
+       
     }
 }
 int main()
 {
     vector<vector<char>> board;
-    int n = 2;
-    for(int i=0;i<n;i++){
-        vector <char> newRow;
-       for(int j=0;j<n;j++) {
-        newRow.push_back('.');
-       }
-       board.push_back(newRow);
+    int n = 4;
+    for (int i = 0; i < n; i++)
+    {
+        vector<char> newRow;
+        for (int j = 0; j < n; j++)
+        {
+            newRow.push_back('.');
+        }
+        board.push_back(newRow);
     }
-    nQueens(board,0);
+    nQueens(board, 0);
     return 0;
 }
