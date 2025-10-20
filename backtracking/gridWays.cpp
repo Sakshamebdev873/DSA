44aec91122f3800b99ebe2dec7e85576415e1729
Sakshamebdev873 <vinodarya344@gmail.com>
2025-10-20 19:37:11 +0530

    fixed with path on gridways

--- backtracking/gridWays.cpp
+++ backtracking/gridWays.cpp
@@ -2,22 +2,25 @@
 #include <string>
 #include <vector>
 using namespace std;
-int gridWays(int r,int c,int n,int m){
-    if(r==n-1 && c==m-1){
+int gridWays(int r, int c, int n, int m, string path) {
+    if (r == n - 1 && c == m - 1) {
+        cout << path << endl; // print complete path
         return 1;
     }
-    if(r>=n || c>=m){
+    if (r >= n || c >= m) {
         return 0;
     }
-    int val1 = gridWays(r,c+1,n,m);
-    int val2 = gridWays(r+1,c,n,m);
-    return val1+val2;
+
+    // Move Right
+    int val1 = gridWays(r, c + 1, n, m, path + "R");
+
+    // Move Down
+    int val2 = gridWays(r + 1, c, n, m, path + "D");
+
+    return val1 + val2;
 }
 
-int main(){
-    int n =4;
-    int m =4;
-    // src=0,0 and dest is n,m
-    cout<<gridWays(0,0,n,m);
-    return 0;
+int main() {
+    int n = 2, m = 2;
+    cout << "Total ways: " << gridWays(0, 0, n, m, "") << endl;
 }
