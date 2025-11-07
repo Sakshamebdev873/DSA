ec80444b98d68c83ba2475d2159eb2a2684c8ca0
Sakshamebdev873 <vinodarya344@gmail.com>
2025-11-08 01:04:28 +0530

    stack basixc implementation

--- Stacks/stack.cpp
+++ Stacks/stack.cpp
@@ -1,6 +1,45 @@
 #include <iostream>
+#include <vector>
 using namespace std;
-int main(){
-    
+
+class Stack {
+    vector<int> vec;
+
+public:
+    void push(int val) {
+        vec.push_back(val);
+    }
+
+    void pop() {
+        if (isEmpty()) {
+            cout << "Stack is empty" << endl;
+            return;
+        }
+        vec.pop_back();
+    }
+
+    int print() {
+        int lastIdx = vec.size() - 1;
+        return vec[lastIdx];
+    }
+
+    bool isEmpty() {
+        return vec.empty();
+    }
+};
+
+int main() {
+    Stack s;
+    s.push(10);
+    s.push(12);
+    s.push(34);
+    s.push(45);
+    s.push(67);
+
+    while (!s.isEmpty()) {
+        cout << s.print() << endl;
+        s.pop();
+    }
+
     return 0;
 }
