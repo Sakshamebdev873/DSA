741fe0f745c6b986d058bdadec7925ba92df4493
Sakshamebdev873 <vinodarya344@gmail.com>
2025-11-23 00:08:17 +0530

    code of centring it

--- Codeforces/p3.cpp
+++ Codeforces/p3.cpp
@@ -1,30 +1,50 @@
 #include <iostream>
 using namespace std;
 
+int moves = 0;
 
-int beautiful(int arr[][5],int i,int j){
-   
+void beautiful(int arr[5][5], int &i, int &j) {
+    // center is (2,2)
+    while (i != 2) {
+        if (i > 2) {
+            swap(arr[i][j], arr[i-1][j]);
+            i--;
+        } else {
+            swap(arr[i][j], arr[i+1][j]);
+            i++;
+        }
+        moves++;
+    }
+
+    while (j != 2) {
+        if (j > 2) {
+            swap(arr[i][j], arr[i][j-1]);
+            j--;
+        } else {
+            swap(arr[i][j], arr[i][j+1]);
+            j++;
+        }
+        moves++;
+    }
 }
-int main()
-{
-    int arr[][5] = {0};
-// input of loop
-    for (int i = 0; i < 5; i++)
-    {
-        for (int j = 0; j < 5; j++)
-        {
+
+int main() {
+    int arr[5][5];
+    int r, c;
+
+    // input
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
             cin >> arr[i][j];
-        }
-        cout << endl;
-    }
-// now making it beautiful
-for(int i=0;i<5;i++){
-    for(int j=0;j<5;j++){
-        if(arr[i][j] == 1){
-            beautiful(arr,i,j);
-            return;
+            if (arr[i][j] == 1) {
+                r = i;
+                c = j;
+            }
         }
     }
-}
+
+    beautiful(arr, r, c);
+
+    cout << moves;
     return 0;
 }
