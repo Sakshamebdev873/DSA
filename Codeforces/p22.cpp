fd285cc01d2825c52031339144425b8ae632736b
Sakshamebdev873 <vinodarya344@gmail.com>
2025-11-29 23:59:33 +0530

    doing

---
+++ Codeforces/p22.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    // a se jyada nhi hona chahiye chaiye n or b ka multiple 
+    int a;
+    cin>>a;
+    int n;
+    cin>>n;
+    int b;
+    cin>>b;
+    int t;
+    cin>>t;
+    while (t--)
+    {
+       
+    }
+    
+    return 0;
+}
