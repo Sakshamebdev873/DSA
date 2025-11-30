0fd7eeffc500c4685e1e22a006d7fde7672b5e4b
Sakshamebdev873 <vinodarya344@gmail.com>
2025-11-30 10:45:07 +0530

    tab closing moves

--- Codeforces/p22.cpp
+++ Codeforces/p22.cpp
@@ -2,19 +2,27 @@
 using namespace std;
 int main()
 {
-    // a se jyada nhi hona chahiye chaiye n or b ka multiple 
-    int a;
-    cin>>a;
-    int n;
-    cin>>n;
-    int b;
-    cin>>b;
+    // a se jyada nhi hona chahiye chaiye n or b ka multiple b hai difference tumhara
+
     int t;
-    cin>>t;
+    cin >> t;
     while (t--)
     {
-       
+        int moves = 0;
+        long long a, b, n;
+        cin>> a >> b >> n;
+
+        if ((n * b) <= a  || b >= a)
+        {
+            moves = 1;
+        }
+        
+        else
+        {
+            moves = 2;
+        }
+        cout << moves << endl;
     }
-    
+
     return 0;
 }
---
+++ test.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a = 36;
+    int b = 7;
+    int m = 5;
+    int ans  = min(b,a/m);
+    cout<<ans;
+    return 0;
+}
