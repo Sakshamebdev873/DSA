5d39529d528838093b98c68a9a90b66195929d9f
Sakshamebdev873 <vinodarya344@gmail.com>
2025-12-16 00:52:14 +0530

    BubbleSort

---
+++ BubbleSort.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+void print(int arr[],int n){
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+void BubbleSort(int arr[],int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-i-1;j++){
+            if(arr[j]>arr[j+1]){
+                swap(arr[j],arr[j+1]);
+            }
+        }
+    }
+}
+int main(){
+    int arr[] = {2,1,34,53,21,32};
+    int size = sizeof(arr)/sizeof(arr[0]);
+// Before
+print(arr,size);
+BubbleSort(arr,size);
+// after
+print(arr,size);
+    return 0;
+}
